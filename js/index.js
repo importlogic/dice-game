7bd2b8e8acd812da12faadb07ae843e871ce25b5
Manas Rawat <manasrawat.cse@gmail.com>
2022-06-15 17:20:44 +0530

    favicon added, dice animation added

--- js/index.js
+++ js/index.js
@@ -5,14 +5,26 @@ var diceTwo = document.querySelector(".dice-two");
 
 button.addEventListener("click", rollDice);
 
-function rollDice(){
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+ }
+
+async function rollDice(){
+    button.setAttribute("disabled", "");
+    button.setAttribute("aria-disabled", "true");
+
+    diceOne.setAttribute("src", "https://c.tenor.com/i_L5KauoCcoAAAAi/dice.gif");
+    diceTwo.setAttribute("src", "https://c.tenor.com/i_L5KauoCcoAAAAi/dice.gif");
+
     var a = Math.floor((Math.random() * 6) + 1);
     var b = Math.floor((Math.random() * 6) + 1);
 
     var one = "images/dice" + a + ".png";
     var two = "images/dice" + b + ".png";
 
+    await sleep(1000);
     diceOne.setAttribute("src", one);
+    await sleep(700);
     diceTwo.setAttribute("src", two);
 
     if(a > b){
@@ -24,4 +36,7 @@ function rollDice(){
     else{
         heading.innerText = "Draw!!";
     }
+
+    button.removeAttribute("disabled");
+    button.removeAttribute("aria-disabled");
 }
